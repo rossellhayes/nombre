4de5ecf2ad8fc8c70284102aa094db3205fe05dd
Alex Rossell Hayes <alexander@rossellhayes.com>
2020-05-20 11:56:09 -0700

    Add example of `max_n()`

--- examples/cardinal.R
+++ examples/cardinal.R
@@ -4,8 +4,7 @@ nom_card(2 + 4/9)
 nom_card(-2)
 nom_card(-2, negative = "minus")
 
-nom_card(525600)
-paste("There are", nom_card(525600), "minutes in a year.")
+nom_card(5:15, max_n = 10)
 
-nom_card(3.72e13)
+paste("There are", nom_card(525600), "minutes in a year.")
 paste("There are", nom_card(3.72e13), "cells in the human body.")
