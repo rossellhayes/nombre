aa89c0198571235491b61c2ac356f2ddadf1ae26
Alex Rossell Hayes <alexander@rossellhayes.com>
2020-05-20 14:59:31 -0700

    Ensure tests actually use `collective()`

--- tests/testthat/test-collective.R
+++ tests/testthat/test-collective.R
@@ -5,17 +5,17 @@ test_that("simple collective", {
   expect_equal(nom_coll(4), "all four")
 })
 
-test_that("adverbial vector", {
-  expect_equal(nom_adv(1:3), c("the", "both", "all three"))
+test_that("collective vector", {
+  expect_equal(nom_coll(1:3), c("the", "both", "all three"))
 })
 
-test_that("adverbial max_n", {
-  expect_equal(nom_adv(9:10, max_n = 9), c("all nine", "all 10"))
-  expect_equal(nom_adv(1:3, max_n = 1), c("the", "both", "all 3"))
+test_that("collective max_n", {
+  expect_equal(nom_coll(9:10, max_n = 9), c("all nine", "all 10"))
+  expect_equal(nom_coll(1:3, max_n = 1), c("the", "both", "all 3"))
 })
 
 test_that("early return", {
-  expect_equal(nom_ord(numeric(0)), character(0))
+  expect_equal(nom_coll(numeric(0)), character(0))
 })
 
 test_that("errors", {
