f11cef20592b8a3ed50c1fcb9deb5ad75b84ac25
Alex Rossell Hayes <alexander@rossellhayes.com>
2020-05-20 13:49:57 -0700

    Remove commented code

--- R/cardinal.R
+++ R/cardinal.R
@@ -117,44 +117,6 @@ nom_card <- cardinal
 #' @rdname cardinal
 #' @export
 
-# numerator <- function(x, negative = getOption("nombre.negative", "negative")) {
-#   if (!length(x))         return(character(0))
-#   if (!is.numeric(x))     stop("`x` must be numeric")
-#   if (any(x != as.integer(x)))
-#     stop("`x` must be an integer or a double with no decimal component")
-#   if (!is.character(negative)) stop("`negative` must be of type character")
-#   if (length(negative) != 1)   stop("`negative` must be length one")
-#
-#   length <- length(x)
-#
-#   minus        <- rep("", length)
-#   minus[x < 0] <- paste0(negative, " ")
-#   x            <- abs(x)
-#
-#   x      <- format(x, scientific = FALSE)
-#   nchar  <- ceiling(nchar(x[[1]]) / 3) * 3
-#   x      <- format(x, justify = "right", width = nchar, scientific = FALSE)
-#
-#   segment <- matrix(rep(x, nchar / 3), ncol = length, byrow = TRUE)
-#   segment <- substr(
-#     segment, rep(seq(1, nchar, 3), length), rep(seq(3, nchar, 3), length)
-#   )
-#   segment <- convert_hundreds(segment)
-#
-#   nrow    <- nrow(segment)
-#   power   <- segment
-#   power[] <- rep(powers[seq_len(nrow)], length)
-#   power   <- power[nrow:1, ]
-#   power[segment == ""] <- ""
-#
-#   segment[]  <- paste0(segment, power)
-#   x          <- apply(segment, 2, paste, collapse = "")
-#   x[x == ""] <- "zero"
-#
-#   x <- paste0(minus, trimws(x))
-#   x
-# }
-
 numerator <- function(
   x,
   max_n     = getOption("nombre.max_n", Inf),
