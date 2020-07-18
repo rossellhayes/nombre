93211e4979bb44e25c76dc52eab8ed56dbd1e082
Alex Rossell Hayes <alexander@rossellhayes.com>
2020-07-18 14:17:04 -0700

    Refactor decimal_to_fraction() to render terminating fractions with denominators greater than 10 as fractions over a power of ten (e.g. 0.05 -> 5/100 rather than 1/20)

--- src/decimal_to_fraction.cpp
+++ src/decimal_to_fraction.cpp
@@ -1,51 +1,66 @@
 #include <Rcpp.h>
 using namespace Rcpp;
-
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
-
 // [[Rcpp::export]]
 IntegerVector decimal_to_fraction(double x) {
-  if ((0 > x) | (x > 1)) {stop("`x` must be between 0 and 1");}
+  if ((x <= 0) | (x >= 1)) {stop("`x` must be between 0 and 1");}
 
-  int a_guess, b_guess;
-  int a_min, b_min;
-  int a_max, b_max;
-  int a_save, b_save;
-  int i;
+  int numerator         = 0;
+  int numerator_guess   = 0;
+  int numerator_min     = 0;
+  int numerator_max     = 1;
+  int denominator       = 1;
+  int denominator_guess = 1;
+  int denominator_min   = 1;
+  int denominator_max   = 1;
+  int i                 = 5;
+  double fraction       = 0;
+  double fraction_guess = 0;
 
-  a_guess = a_min = a_save = 0;
-  a_max = b_guess = b_min = b_max = b_save = i = 1;
+  if (x - 0 < sqrt(DBL_EPSILON)) {
+    numerator   = 0;
+    denominator = 1e7;
+    fraction    = x;
+  } else if (1 - x < sqrt(DBL_EPSILON)) {
+    numerator   = 1e7;
+    denominator = 1e7;
+    fraction    = x;
+  } else while ((i < 1e7) & (abs(x - fraction) > sqrt(DBL_EPSILON))) {
+    denominator_guess = i;
+    numerator_guess   = (int) round(x * denominator_guess);
+    fraction_guess    = (double) numerator_guess / denominator_guess;
 
-  while (abs(x - (double)a_save / b_save) > DBL_EPSILON) {
-    if (i++ % 100 == 0) {Rcpp::checkUserInterrupt();}
-
-    if (abs(x - (double)a_guess / b_guess) < abs(x - (double)a_save / b_save)) {
-      a_save = a_guess;
-      b_save = b_guess;
+    if (abs(x - fraction_guess) < abs(x - fraction)) {
+      numerator   = numerator_guess;
+      denominator = denominator_guess;
+      fraction    = (double) numerator / denominator;
     }
 
-    a_guess = a_min + a_max;
-    b_guess = b_min + b_max;
+    i = (i == 5) * 2 + (i < 5) * (i * 2) + (i == 8) * 10 + (i >= 10) * (i * 10);
+  }
 
-    if (a_guess > (double)x * b_guess) {
-      a_max = a_guess;
-      b_max = b_guess;
+  denominator_guess = 1;
+
+  while (abs(x - fraction) > sqrt(DBL_EPSILON)) {
+    numerator_guess   = numerator_min   + numerator_max;
+    denominator_guess = denominator_min + denominator_max;
+    fraction_guess    = (double) numerator_guess / denominator_guess;
+
+    if (fraction_guess > x) {
+      numerator_max   = numerator_guess;
+      denominator_max = denominator_guess;
     } else {
-      a_min = a_guess;
-      b_min = b_guess;
+      numerator_min   = numerator_guess;
+      denominator_min = denominator_guess;
+    }
+
+    if (abs(x - fraction_guess) < abs(x - fraction)) {
+      numerator   = numerator_guess;
+      denominator = denominator_guess;
+      fraction    = (double) numerator / denominator;
     }
   }
 
-  IntegerVector result = IntegerVector::create(a_save, b_save);
-  result.attr("dim") = Dimension(2, 1);
+  IntegerVector result = IntegerVector::create(numerator, denominator);
+  result.attr("dim")   = Dimension(2, 1);
   return result;
 }
-
