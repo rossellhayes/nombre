b2cea80d16b035755da506f83f18f81a7a5c6ba4
Alex Rossell Hayes <alexander@rossellhayes.com>
2020-07-19 13:20:21 -0700

    Refactor adverbial()

--- R/adverbial.R
+++ R/adverbial.R
@@ -4,7 +4,7 @@
 #' @param thrice A logical of length one.
 #'     If `TRUE`, the adverbial of `3` will be "thrice".
 #'     If `FALSE`, the adverbial of `3` will be "three times".
-#'     Defaults to `TRUE`.
+#'     Defaults to `FALSE`.
 #'     Default can be changed by setting `options("nombre.thrice")`.
 #' @param ... Additional arguments passed to [cardinal()]
 #'
@@ -14,20 +14,20 @@
 #' @example examples/adverbial.R
 
 adverbial <- function(x, thrice = getOption("nombre.thrice", FALSE), ...) {
-  if (!length(x))           return(character(0))
-  if (!is.numeric(x))       stop("`x` must be a numeric vector")
+  if (!length(x))          return(character(0))
+  if (!is.numeric(x))      stop("`x` must be a numeric vector")
   if (length(thrice) != 1) stop("`thrice` must be length one")
   if (!is.logical(thrice) | is.na(thrice))
     stop("`thrice` must be either `TRUE` or `FALSE`")
 
-  adv              <- paste(cardinal(x, ...), "time")
-  adv[abs(x) != 1] <- paste0(adv[abs(x) != 1], "s")
+  irregulars <- 1:2
+  if (thrice) {irregulars[[3]] <- 3}
 
-  adv[abs(x) == 1] <- gsub("one time$", "once", adv[abs(x) == 1])
-  adv[abs(x) == 2] <- gsub("two times$", "twice", adv[abs(x) == 2])
-
-  if (thrice)
-    adv[abs(x) == 3] <- gsub("three times$", "thrice", adv[abs(x) == 3])
+  adv                           <- cardinal(x, ...)
+  adv[!abs(x) %in% irregulars]  <- paste(adv[!abs(x) %in% irregulars], "times")
+  adv[abs(x) == 1]              <- gsub("one", "once", adv[abs(x) == 1])
+  adv[abs(x) == 2]              <- gsub("two", "twice", adv[abs(x) == 2])
+  if (thrice) {adv[abs(x) == 3] <- gsub("three$", "thrice", adv[abs(x) == 3])}
 
   adv
 }
