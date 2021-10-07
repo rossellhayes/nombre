120209a743fb6f4d54dcef50cc839905fbd8aa43
Eli Pousson <eli.pousson@gmail.com>
2021-10-06 22:35:00 -0400

    Add `sep` to `cardinal` for converting decimals to ratios/proportions

--- R/convert_fraction.R
+++ R/convert_fraction.R
@@ -1,9 +1,14 @@
-convert_fraction <- function(x, ...) {
+convert_fraction <- function(x, sep, ...) {
   if (!length(x)) return(character(0))
 
   x           <- fracture::frac_mat(x, ...)
   numerator   <- numerator(x[1, ])
-  denominator <- denominator(x[2, ], x[1, ])
 
-  paste(numerator, denominator)
+  if (is.null(sep)) {
+    denominator <- denominator(x[2, ], x[1, ])
+    paste(numerator, denominator)
+  } else {
+    denominator <- numerator(x[2, ])
+    paste(numerator, sep, denominator)
+  }
 }
