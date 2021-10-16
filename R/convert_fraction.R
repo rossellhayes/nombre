1d639f4e185113da5c3c1ab187142ea28720a8f0
Alex Rossell Hayes <alexander@rossellhayes.com>
2021-10-16 14:51:13 -0700

    Reimplement `convert_fraction()`

--- R/convert_fraction.R
+++ R/convert_fraction.R
@@ -1,14 +1,28 @@
-convert_fraction <- function(x, sep = NULL, ...) {
+convert_fraction <- function(
+  x, sep = NULL, max_n = Inf, negative = "negative", ...
+) {
   if (!length(x)) return(character(0))
 
-  x           <- fracture::frac_mat(x, ...)
-  numerator   <- numerator(x[1, ])
+  x <- fracture::frac_mat(x, ...)
 
   if (is.null(sep)) {
-    denominator <- denominator(x[2, ], x[1, ])
-    paste(numerator, denominator)
-  } else {
-    denominator <- numerator(x[2, ])
-    paste(numerator, sep, denominator)
+    numerator   <- numerator(x[1, ], max_n = max_n, negative = negative)
+    denominator <- denominator(x[2, ], x[1, ], max_n = max_n)
+    return(paste(numerator, denominator))
   }
+
+  x[] <- cardinal(
+    x,
+    max_n = if (length(max_n) == 1) {
+      max_n
+    } else {
+      rep(max_n, each = length(x) / 2)
+    },
+    negative = if (length(negative) == 1) {
+      negative
+    } else {
+      rep(negative, each = length(x) / 2)
+    },
+  )
+  paste(x[1, ], sep, x[2, ])
 }
